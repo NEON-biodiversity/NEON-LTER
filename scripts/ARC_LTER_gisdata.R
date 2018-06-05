436f757962f4b74c2b3a9b6a406d056240c5ddd8
cameochilcutt <39775116+cameochilcutt@users.noreply.github.com>
2018-06-05 14:26:06 -0500

    Overlay a shapefile map on a GoogleMap

--- scripts/ARC_LTER_gisdata.R
+++ scripts/ARC_LTER_gisdata.R
@@ -11,77 +11,66 @@ rm(list = ls())
 #libraries
 library(raster)
 library(rgdal)
+library(ggplot2)
+library (ggmap)
+library (ggsn)
 
 #import Camp Buildings 2010 shapefiles
-#shape_path_CB10 <- 
-#shape_name_CB10 <- "campbuilds_2010_100909"
-shape_extent_CB10 <- readOGR(".\\arc\\gis_data\\camp_buildings$data\\Camp Buildings", "campbuilds_2010_100909")
+CB10 <- readOGR(".\\arc\\gis_data\\camp_buildings$data\\Camp Buildings", "campbuilds_2010_100909")
 
 #plot Camp Buildings 2010
-plot(shape_extent_CB10,  col=terrain.colors(10),
-     alpha=1,
-     legend=F, main="Camp Buildings 2010")
+plot(CB10,  col=terrain.colors(10), alpha=1, legend=F, main="Camp Buildings 2010")
 
 #Reprojection
-mapCamp10<-spTransform(shape_extent_CB10, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapCamp10<-spTransform(CB10, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(mapCamp10)
 mapCamp10
 
 #import Camp Buildings 2013
-shape_path_CB13 <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\camp_buildings_2013$data"
-shape_name_CB13 <- "Camp_Buildings_2013"
-shape_extent_CB13 <- readOGR(shape_path_CB13, shape_name_CB13)
+CB13 <- readOGR(".\\arc\\gis_data\\camp_buildings_2013$data", "Camp_Buildings_2013")
 
 #plot Camp Buildings 2013
-plot(shape_extent_CB13, col=rainbow(10),
+plot(B13, col=rainbow(10),
      alpha=1,
      legend=F, main="Camp Buildings 2013")
 
 #Reprojection
-mapCamp13<-spTransform(shape_extent_CB13, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapCamp13<-spTransform(CB13, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(mapCamp13)
 mapCamp13
 
 #import Gravel Pads
-shape_path_GP <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\gravel_pads$data"
-shape_name_GP <- "gravel_pads"
-shape_extent_GP <- readOGR(shape_path_GP, shape_name_GP)
+GravelPads <- readOGR(".\\arc\\gis_data\\gravel_pads$data", "gravel_pads")
 
 #plot Gravel Pads
-plot(shape_extent_GP, col=grey(1:100/100), legend=FALSE, main="Gravel Pads")
+plot(GravelPads, col=grey(1:100/100), legend=FALSE, main="Gravel Pads")
 
 #Reprojection
-map_GP<-spTransform(shape_extent_GP, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+map_GP<-spTransform(GravelPads, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(map_GP)
 map_GP
 
 #import hyd_toolik_arc
-shape_path_Hydtoolarc <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\hyd_toolik_arc_101111$data"
-shape_name_Hydtoolarc <- "hyd_toolik_arc101111"
-shape_extent_Hydtoolarc <- readOGR(shape_path_Hydtoolarc, shape_name_Hydtoolarc)
+Hydtoolarc <- readOGR(".\\arc\\gis_data\\hyd_toolik_arc_101111$data", "hyd_toolik_arc101111")
 
 #plot hyd_toolik_arc
-plot(shape_extent_Hydtoolarc, col=rainbow(4),
+plot(Hydtoolarc, col=rainbow(4),
      alpha=1,
      legend=F, main="Hyd_toolik_arc")
 
 #Reprojection
-maptoolikarc<-spTransform(shape_extent_Hydtoolarc, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+maptoolikarc<-spTransform(Hydtoolarc, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(maptoolikarc)
 maptoolikarc
 
 #import hyd_toolik_poly
-shape_path_Hydtoolpoly <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\hyd_toolik_poly101111$data\\files for website"
-shape_name_Hydtoolpoly <- "hyd_toolik_poly101111"
-shape_extent_Hydtoolpoly <- readOGR(shape_path_Hydtoolpoly, shape_name_Hydtoolpoly)
+Hydtoolpoly<- readOGR(".\\arc\\gis_data\\hyd_toolik_poly101111$data", "hyd_toolik_poly101111")
 
 #plot hyd_toolik_poly
-plot(shape_extent_Hydtoolpoly, col=cm.colors(10),
-     alpha=1,
-     legend=F, main="Hyd_toolik_poly")
+plot(Hydtoolpoly, col=cm.colors(10), alpha=1, legend=F, main="Hyd_toolik_poly")
 
 #Reprojection
-maptoolikpoly<-spTransform(shape_extent_Hydtoolpoly, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+maptoolikpoly<-spTransform(Hydtoolpoly, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(maptoolikpoly)
 maptoolikpoly
 
@@ -94,125 +83,122 @@ maptoolikpoly
 #plot(shape_extent_5,  col=grey(1:100/100), legend=FALSE, main="Research Plots 2009")
 
 #import roads_trails$data
-shape_path_Roads <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\roads_trails$data"
-shape_name_Roads <- "transport_100423"
-shape_extent_Roads <- readOGR(shape_path_Roads, shape_name_Roads)
+Roads<- readOGR(".\\arc\\gis_data\\roads_trails$data", "transport_100423")
+
 
 #plot roads_trails$data
-plot(shape_extent_Roads, col=cm.colors(10),
-     alpha=1,
-     legend=F, main="Road_Trails")
+plot(Roads, col=cm.colors(10), alpha=1, legend=F, main="Road_Trails")
 
 #Reprojection
-mapTrails<-spTransform(shape_extent_Roads, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapTrails<-spTransform(Roads, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(mapTrails)
 mapTrails
 
 #import taps$data
-shape_path_Taps <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\taps$data"
-shape_name_Taps <- "pipeline_proj"
-shape_extent_Taps <- readOGR(shape_path_Taps, shape_name_Taps)
+Taps<- readOGR(".\\arc\\gis_data\\taps$data", "pipeline_proj")
 
 #plot taps$data
-plot(shape_extent_Taps, col=heat.colors(5),
-     alpha=1,
-     legend=F, main="Taps")
+plot(Taps, col=heat.colors(5), alpha=1, legend=F, main="Taps")
 
 #Reprojection
-maptaps<-spTransform(shape_extent_Taps, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+maptaps<-spTransform(Taps, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(maptaps)
 maptaps
 
 #import toolik_rna$data
-shape_path_ToolikRNA <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\toolik_rna$data"
-shape_name_ToolikRNA <- "Toolik_RNA"
-shape_extent_ToolikRNA <- readOGR(shape_path_ToolikRNA, shape_name_ToolikRNA)
+ToolikRNA <- readOGR(".\\arc\\gis_data\\toolik_rna$data", "Toolik_RNA")
 
 #plot toolik_rna$data
-plot(shape_extent_ToolikRNA, col=terrain.colors(8),
+plot(ToolikRNA, col=terrain.colors(8),
      alpha=1,
      legend=F, main="Toolik_RNA")
 
 #Reprojection
-mapToolik<-spTransform(shape_extent_ToolikRNA, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapToolik<-spTransform(ToolikRNA, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 plot(mapToolik)
 mapToolik
 
 #import watersheds_research
-shape_path_Water <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\watersheds_research$data\\Watersheds_Research"
-shape_name_Water <- "Crump_Watershed"
-shape_extent_Water <- readOGR(shape_path_Water, shape_name_Water)
+CrumpWater <- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Crump_Watershed")
+ImnaviatWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Imnaviat_Watershed")
+KlingWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Kling_Watershed")
+KuparukWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Kuparuk_Watershed")
+LostLakeWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Lost_Lake_Watershed")
+OksrukuyikWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Oksrukuyik_Watershed")
+ThermokarstWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Thermokarst_Watershed")
+ToolikinletWater<- readOGR(".\\arc\\gis_data\\watersheds_research$data\\Watersheds_Research", "Toolik_inlet_Watershed")
 
 #plot watersheds research
-plot(shape_extent_Water, col=cm.colors(8),
-     alpha=1,
-     legend=F, main="Watersheds Research")
+plot(CrumpWater, col=cm.colors(8), alpha=1, legend=F, main="Crump Watersheds Research")
+plot(ImnaviatWater, col=cm.colors(8), alpha=1, legend=F, main="Imnaviat Watersheds Research")
+plot(KlingWater, col=cm.colors(8), alpha=1, legend=F, main="Kling Watersheds Research")
+plot(KuparukWater, col=cm.colors(8), alpha=1, legend=F, main="Kuparuk Watersheds Research")
+plot(LostLakeWater, col=cm.colors(8), alpha=1, legend=F, main="Lost Lake Watersheds Research")
+plot(OksrukuyikWater, col=cm.colors(8), alpha=1, legend=F, main="Oksrukuyik Watersheds Research")
+plot(ThermokarstWater, col=cm.colors(8), alpha=1, legend=F, main="Thermokarst Watersheds Research")
+plot(ToolikinletWater, col=cm.colors(8), alpha=1, legend=F, main="Toolik inlet Watersheds Research")
 
 #Reprojection
-mapWater<-spTransform(shape_extent_Water, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(mapWater)
-mapWater
+mapCrumpWater<-spTransform(CrumpWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapImnaviatWater<-spTransform(ImnaviatWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapKlingWater<-spTransform(KlingWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapKuparukWater<-spTransform(KuparukWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapLostLakeWater<-spTransform(LostLakeWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapOksrukuyikWater<-spTransform(OksrukuyikWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapThermokarstWater<-spTransform(ThermokarstWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+mapToolikinletWater<-spTransform(ToolikinletWater, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+plot(mapCrumpWater)
+plot(mapImnaviatWater)
+plot(mapKlingWater)
+plot(mapKuparukWater)
+plot(mapLostLakeWater)
+plot(mapOksrukuyikWater)
+plot(mapThermokarstWater)
+plot(mapToolikinletWater)
 
-##########################################################################################################
-#Krymsen's Part
-shape_path <- "C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\100fcont_uk$data"
-shape_name<-"100fcont_uk"
-shape_extent<-readOGR(shape_path, shape_name)
-plot(shape_extent, col=grey(1:100/100), main="100f\n NEON Harvard Forest Field Site")
+#100fcont_uk
+fcont_uk<-readOGR(".\\arc\\gis_data\\100fcont_uk$data", "100fcont_uk")
+plot(fcont_uk, col=grey(1:100/100), main="fcont_uk")
 
-#Reprojection
-map100fcont<-spTransform(shape_extent, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(map100fcont)
-map100fcont
+#Reprojection **Not working**
+#mapfcont<-spTransform(fcont_uk, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+#plot(mapfcont)
+#map100fcont
 
-shape_path_1<-"C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\anak_burn_lakes$data"
-shape_name_1<-"Anaktuvuk_Burn_Lakes"
-shape_extent_1<-readOGR(shape_path_1, shape_name_1)
-plot(shape_extent_1, col=heat.colors (10), main="Burn_Lakes\n NEON Harvard Forest Field Site")
-
-#Reprojection
-mapanak_burn_lakes<-spTransform(shape_extent_1, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(mapanak_burn_lakes)
-mapanak_burn_lakes
-
-shape_path_2<-"C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\anak_burn_perim_rocha$data"
-shape_name_2<-"Anak_Burn_Perim_Rocha"
-shape_extent_2<-readOGR(shape_path_2,shape_name_2)
-plot(shape_extent_2, col=grey(1:100/100), main="Anak_Burn_Perim\n NEON Harvard Forest Field Site")
-
-#Reprojection
-mapanak_burn_perim<-spTransform(shape_extent_2, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(mapanak_burn_perim)
-mapanak_burn_perim
-
-shape_path_3<-"C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\anak_burn_rivers$data"
-shape_name_3<-"Anaktuvuk_Burn_Rivers"
-shape_extent_3<-readOGR(shape_path_3, shape_name_3)
-plot(shape_extent_3, col=grey(1:100/100), main="Anak_Burn_river\n NEON Harvard Forest Field Site")
+#Anak_Burn_Lakes
+AnakBurnLakes<-readOGR(".\\arc\\gis_data\\anak_burn_lakes$data", "Anaktuvuk_Burn_Lakes")
+plot(AnakBurnLakes, col=heat.colors (10), main="Burn_Lakes\n NEON Harvard Forest Field Site")
 
 #Reprojection
-mapanak_burn_rivers<-spTransform(shape_extent_3, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(mapanak_burn_rivers)
-mapanak_burn_rivers
+mapAnakBurnLakes<-spTransform(AnakBurnLakes, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+plot(mapAnakBurnLakes)
 
-shape_path_4<-"C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\anaktuvuk_burn_perim$data"
-shape_name_4<-"progression_perimeters_0822-0930"
-shape_extent_4<-readOGR(shape_path_4, shape_name_4)
-plot(shape_extent_4, col=grey(1:100/100), main="Progression_perimeters\n NEON Harvard Forest Field Site")
+#Anak_Burn_Perim_Rocha
+AnakBurnPerimRocha<-readOGR(".\\arc\\gis_data\\anak_burn_perim_rocha$data", "Anak_Burn_Perim_Rocha")
+plot(AnakBurnPerimRocha, col=grey(1:100/100), main="Anak_Burn_Perim\n NEON Harvard Forest Field Site")
 
 #Reprojection
-mapanaktuvuk_burn_perim<-spTransform(shape_extent_4, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-plot(mapanaktuvuk_burn_perim)
-mapanaktuvuk_burn_perim
-########################################################################################################
-#Download packages ggplot and ggmap
+mapAnakBurnPerimRocha<-spTransform(AnakBurnPerimRocha, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+plot(mapAnakBurnPerimRocha)
 
-#Load in libraries
-library(ggplot2)
-library (ggmap)
+#Anak_Burn_Rivers
+AnakBurnRivers<-readOGR(".\\arc\\gis_data\\anak_burn_rivers$data", "Anaktuvuk_Burn_Rivers")
+plot(AnakBurnRivers, col=grey(1:100/100), main="Anak_Burn_River\n NEON Harvard Forest Field Site")
+
+#Reprojection
+mapAnakBurnRivers<-spTransform(AnakBurnRivers, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+plot(mapAnakBurnRivers)
+
+#Anaktuvuk_Burn_Perim
+AnaktuvukBurnPerim<-readOGR(".\\arc\\gis_data\\anaktuvuk_burn_perim$data", "progression_perimeters_0822-0930")
+plot(AnaktuvukBurnPerim, col=grey(1:100/100), main="Progression_perimeters\n NEON Harvard Forest Field Site")
+
+#Reprojection
+mapAnaktuvukBurnPerim<-spTransform(AnaktuvukBurnPerim, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+plot(mapAnaktuvukBurnPerim)
 ############################################################################################################
 #BaseMap of Arctic Field Site
-Arctic_Map<-raster("C:\\Users\\Cameo Chilcutt\\Documents\\NEON\\nasa_modis_2013$data\\alaska_tmo_2013168_geo.tif")
+Arctic_Map<-raster("G:\\My Drive\\NEON_LTER_2018\\data\\raw_data\\arc\\gis_data\\nasa_modis_2013$data\\alaska_tmo_2013168_geo.tif")
 plot(Arctic_Map)
 #########################################################################################
 #Clipping satellite image to size of Toolik research area
@@ -228,18 +214,31 @@ plot(mapCamp10, add=TRUE)
 #Adding titles
 title(main="Camp Buildings 2010 - Alaska Field Site", xlab="Longitude", ylab="Latitude")
 ###############################################################################################
+#Google Base Maps
+##Extent of Toolik Field Station
+##NewExtent<-extent(-149.600, -149.589, 68.625, 68.630)
 
+#Obtain basemap of the Arctic field site through google maps 
+#(can also generate map types "roadmap", "terrain", "satellite", "hybrid")
 
+arctic_map <- get_map(location = c(lon = -150.5, lat = 69.3),
+                      color = "color",
+                      source = "google",
+                      maptype = "terrain",
+                      zoom = 7)
 
-##############################################################################################################
-#importing GEOTIF **Work in progress**
-Alaska<-raster("C:\\Users\\Cameo Chilcutt\\Documents\\ldcoaki0100a.tif")
-plot(Alaska)
-NewExtentAlaska<-extent(-149.600, -149.589, 68.625, 68.630)
-extent(Alaska)<- NewExtentAlaska
-plot(Alaska)
-#plot(mapG, add=TRUE)
+#Visualize the shapefile and make sure it loaded in correctly
+ggplot(data = mapAnaktuvukBurnPerim, aes(x = long, y = lat, group = group)) + geom_path()
 
-#Change resolution of raster layer
-ResampledAlaska<-resample(Alaska, toolik, method="bilinear", filename='ResampledAlaska', format="Gtiff")
-plot(ResampledAlaska)
+#Use the function 'fortify' to turn this shapefile into a dataframe so it's easy to plot on map
+mapAnaktuvukBurnPerim.f = fortify(mapAnaktuvukBurnPerim)
+
+#Generate a full map with both the google map, polygon, north arrow and scale bar.
+ggmap(arctic_map) + 
+  geom_polygon(aes(x = long, y = lat, group = group), 
+               data = mapAnaktuvukBurnPerim.f,
+               alpha = 0.8, 
+               color = "blue",
+               size = 0.2) + xlab("Longitude")+ ylab("Latitude") + ggtitle("Toolik Field Station: Digitized Burn Perimeter")+ theme(plot.title = element_text(hjust = .5)) + scalebar(x.min= -153.3, x.max= -153.0, y.min= 68.35, y.max= 68.42, dist= 50, location= "bottomleft", dd2km = TRUE, st.size=4, st.dist = .6, height = 0.5, model="WGS84")
++ north2(arctic_map, x = 0.30, y = 0.31, scale = 0.15, symbol = 1)
+
