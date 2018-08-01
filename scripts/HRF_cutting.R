4d28ed9298e5136d45af9aaf8a66832bdb1537ed
khoerr <39534997+khoerr@users.noreply.github.com>
2018-08-01 14:47:40 -0400

    Update HRF_cutting.R

--- scripts/HRF_cutting.R
+++ scripts/HRF_cutting.R
@@ -24,11 +24,11 @@ points(hrf_map, col="red")
 
 # Read Mass cutting data
 # Read csvs
-plan <- read.csv("/Mass_Timber/hf080-01-plan.csv", header=TRUE, stringsAsFactors=FALSE)
-volume <- read.csv("/Mass_Timber/hf080-02-volume.csv", header=TRUE, stringsAsFactors=FALSE)
+plan <- read.csv("C:/Users/Kyra/Documents/Harvard/data/HF/LTER/Mass_Timber/hf080-01-plan.csv", header=TRUE, stringsAsFactors=FALSE)
+volume <- read.csv("C:/Users/Kyra/Documents/Harvard/data/HF/LTER/Mass_Timber/hf080-02-volume.csv", header=TRUE, stringsAsFactors=FALSE)
 
 # Read shapefile
-cutting <- readOGR("/Mass_Timber/hf080-03-gis", "all_cutting_plans_v4")
+cutting <- readOGR("C:/Users/Kyra/Documents/Harvard/data/HF/LTER/Mass_Timber/hf080-03-gis", "all_cutting_plans_v4")
 cut_data <- spTransform(cutting, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
 
 # Change cut_data name
@@ -44,8 +44,8 @@ cut_crop <- crop(cut_data, extent(hrf_map))
 plot(cut_crop)
 points(hrf_map, col="red")
 
-# Export cutting map; set dsn to where the file should be saved
-writeOGR(obj=cut_crop, dsn= "", layer="hrf_cutting_map", driver="ESRI Shapefile") # this is in geographical projection
+# Export cutting map
+writeOGR(obj=cut_crop, dsn= "C:/Users/Kyra/Documents/Harvard/data/HF/LTER", layer="hrf_cutting_map", driver="ESRI Shapefile") # this is in geographical projection
 
 # Find HF NEON point data
 hrf_cut <- over(hrf_map,cut_crop)
@@ -55,10 +55,23 @@ length(hrf_cut)
 # Find NEON plots within cutting polygons
 hrf_points <- intersect(hrf_map,cut_crop) # points (35)
 
+#subset from hrf_points
+NEON_cut <- subset(hrf_points, select=c("fis.year","plotID"))
+
 # Find cutting polygons containing NEON points
 hrf_polygons <- intersect(cut_crop,hrf_map) # polygons (25)
 points(hrf_points, col="red")
 
-# Export points and polygons; set dsn to where the file should be saved
-writeOGR(obj=hrf_points, dsn= "", layer="hrf_points", driver="ESRI Shapefile") # this is in geographical projection
-writeOGR(obj=hrf_polygons, dsn= "", layer="hrf_polygons", driver="ESRI Shapefile") # this is in geographical projection
+# Export points and polygons
+writeOGR(obj=hrf_points, dsn= "C:/Users/Kyra/Documents/Harvard/data/HF/LTER", layer="hrf_points", driver="ESRI Shapefile") # this is in geographical projection
+writeOGR(obj=hrf_polygons, dsn= "C:/Users/Kyra/Documents/Harvard/data/HF/LTER", layer="hrf_polygons", driver="ESRI Shapefile") # this is in geographical projection
+
+# Find frequencies of cut types
+as.data.frame(table(hrf_points$typereg1))
+as.data.frame(table(hrf_points$typereg2))
+as.data.frame(table(hrf_points$typereg3))
+
+
+
+# Export hrf_cut for Within_site_prep.R
+write.csv(hrf_cut, file="hrf_cut.csv")
